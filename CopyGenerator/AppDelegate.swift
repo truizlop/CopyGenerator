f182ca6826125c9b5b90682a412862d43848f41c
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-09-25 16:21:46 +0200

    Initial Commit

---
+++ CopyGenerator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CopyGenerator
+//
+//  Created by Tomás Ruiz López on 25/9/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
